185a3a5ffd1051f2bba959e3edb60a1b945a7762
Kat <kat.m.deans@gmail.com>
2025-02-18 15:35:39 -0500

    Main initialization

--- app/(pages)/introduction/page.js
+++ app/(pages)/introduction/page.js
@@ -5,6 +5,14 @@ function Introduction() {
   return (
     <>
         <Navbar/>
+        <main className='relative ml-9 mr-9'>
+            <div className='flex flex-col pb-9'>
+                <h1 className='font-bold'>TO START ANALYSIS</h1>
+                <div className='introduction__square'></div>
+
+            </div>
+
+        </main>
     
     </>
 
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 function Navbar() {
     return (
-        <nav>
-            <div className="flex justify-center "> 
-                <Link href="/">
+        <nav className="flex items-center justify-between top-0 z-3 ml-auto mr-auto py-8 ml-9 mr-9 w-full">
+            <div className="flex items-center relative z-30 "> 
+                <Link href="/" className="font-bold">
                     SKINSTRIC
                 </Link>
             </div>
