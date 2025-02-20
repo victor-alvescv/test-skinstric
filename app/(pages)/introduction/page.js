bedd5ba6d3aed3d1cffbb3832b21e3eaae6d67f9
Kat <kat.m.deans@gmail.com>
2025-02-20 12:45:01 -0500

    input label

--- app/(pages)/introduction/page.js
+++ app/(pages)/introduction/page.js
@@ -7,7 +7,11 @@ function Introduction() {
       <div className="flex flex-col flex-auto">
         <nav className="items-center flex h-16 justify-between left-0 fixed top-0 w-full z-30 mx-auto max-w-[2560px] px-8">
           <div className="flex items-center relative z-30 ">
-            <Link href="/" className="font-bold text-[10px]" style={{ fontSize: "clamp(10px,-2px + .9375vw,16px)" }}>
+            <Link
+              href="/"
+              className="font-bold text-[10px]"
+              style={{ fontSize: "clamp(10px,-2px + .9375vw,16px)" }}
+            >
               SKINSTRIC
             </Link>
           </div>
@@ -25,17 +29,23 @@ function Introduction() {
             <div class="introduction-square js-introduction-square">
               <span class="dotted-square is-expanded is-animated"></span>
             </div>
-
-            <div className="flex items-center h-screen flex-col  justify-center absolute left-1/2 transform -translate-x-1/2 text-center top-1/2 -translate-y-1/2">
-              <div className="font-normal tracking-normal absolute bottom-full mb-2px opacity-40 leading-[1.71] text-[clamp(10px, -6px + 1.5625vw, 14px)]">
-                CLICK TO TYPE
+            <div className="items-center flex flex-col justify-center left-1/2 absolute text-center top-1/2 translate-x-[-50%] translate-y-[-50%]">
+              <div
+                style={{ fontSize: "clamp(10px,-6px + 1.5625vw,14px)" }}
+                className="bottom-[100%] mb-1 opacity-40 absolute uppercase font-normal tracking-[0] leading-[1.71]"
+              >
+                Click to type
               </div>
               <form style={{ clippath: "inset(0%)," }}>
-                    <div className="py-[5px] relative">
-                        <input className="border-b border-solid border-black leading-none text-center text-[#1a1b1c]" style={{ width: "calc((var(--introduction-form-label-symbols, 15) - 5.5)* 1ch)" }}/>
-                    </div>
-
-
+                <div className="py-[5px] relative">
+                  <input
+                    className="border-b border-solid border-black leading-none text-center text-[#1a1b1c]"
+                    style={{
+                      width:
+                        "calc((var(--introduction-form-label-symbols, 15) - 5.5)* 1ch)",
+                    }}
+                  />
+                </div>
               </form>
             </div>
 
