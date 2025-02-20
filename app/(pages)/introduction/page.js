a58d1e45dbf9bb87c43187f8b46c00a9522837c4
Kat <kat.m.deans@gmail.com>
2025-02-19 22:34:47 -0500

    Squares - introduction page - initialization

--- app/(pages)/introduction/page.js
+++ app/(pages)/introduction/page.js
@@ -5,13 +5,15 @@ function Introduction() {
   return (
     <>
         <Navbar/>
-        <main className='relative ml-9 mr-9'>
-            <div className='flex flex-col pb-9'>
-                <h1 className='font-bold'>TO START ANALYSIS</h1>
-                <div className='introduction__square'></div>
-
+        <main className='relative flex flex-auto flex-col inherit-z ml-9 mr-9'>
+            <div className='flex flex-1 flex-shrink basis-auto flex-col pb-9 relative overflow-hidden ml-auto mr-auto max-w-[2560px] pl-offset-x pr-offset-x w-full'>
+                <div className='flex flex-col pb-9 pt-9 absolute left-offset-x overflow-hidden'>
+                    <h1 className='font-bold'>TO START ANALYSIS</h1>
+                </div>
+                <div class="introduction-square js-introduction-square">
+                    <span class="dotted-square is-expanded is-animated"></span>
+                </div>
             </div>
-
         </main>
     
     </>
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 function Navbar() {
     return (
-        <nav className="flex items-center justify-between top-0 z-3 ml-auto mr-auto py-8 ml-9 mr-9 w-full">
+        <nav className="flex items-center justify-between top-0 z-3 py-8 px-9 ml-9 mr-9 w-full">
             <div className="flex items-center relative z-30 "> 
                 <Link href="/" className="font-bold">
                     SKINSTRIC
                 </Link>
-            </div>
-        </nav>
+                </div>
+         </nav>
     )
 }
 
