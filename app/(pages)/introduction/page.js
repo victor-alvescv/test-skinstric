efda2367c3ee3f57593d2ba270841a123999b2a7
Kat <kat.m.deans@gmail.com>
2025-02-25 18:05:37 -0500

    Arrows for the buttons

--- app/(pages)/introduction/page.js
+++ app/(pages)/introduction/page.js
@@ -102,7 +102,7 @@ function Introduction() {
                             <div className="mr-auto flex-none flex-shrink basis-1/4 pr-2.5 text-color-color">
                                 <button onClick={handleBack} className="button__back">
                                         <span className="button__icon--square">
-                                            <span className="button__icon--square-inner">
+                                            <span className="button__icon--square-inner button__icon-left">
                                                 <svg className="w-3 h-3 relative -top-1 left-1 transform rotate-90" viewBox="0 0 11 12" fill="#1A1B1C" xmlns="http://www.w3.org/2000/svg" role="img">
                                                     <path d="M10.145 6 .716 11.444V.556L10.145 6Z" fill="current"></path>
                                                 </svg>
@@ -115,7 +115,7 @@ function Introduction() {
                                 {!showLocation && name && (
                                     <button onClick={handleProceed} className="button__proceed">
                                         <span className="button__icon--square">
-                                            <span className="button__icon--square-inner">
+                                            <span className="button__icon--square-inner button__icon-right">
                                                 <svg className="w-3 h-3 relative -top-1 left-1 transform rotate-90" viewBox="0 0 11 12" fill="#1A1B1C" xmlns="http://www.w3.org/2000/svg" role="img">
                                                     <path d="M10.145 6 .716 11.444V.556L10.145 6Z" fill="current"></path>
                                                 </svg>
