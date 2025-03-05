76af065013bbbfd97b0febc7d23b79dbcd631968
Kat <kat.m.deans@gmail.com>
2025-03-05 12:12:10 -0500

    Update to the photo taking page

--- app/(pages)/aicamera/page.js
+++ app/(pages)/aicamera/page.js
@@ -1,4 +1,5 @@
 "use client";
+
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState, useRef } from "react";
 
@@ -16,6 +17,7 @@ function Aicamera() {
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
+
         console.log("Camera access granted", stream);
       } catch (error) {
         console.error("Camera access denied", error);
@@ -30,12 +32,19 @@ function Aicamera() {
     console.log(cameraAccess);
   }, [cameraAccess]);
 
+  const startCountdown = () => {
+    // Add your countdown logic here
+
+    console.log("Starting countdown...");
+  };
+
   return (
     <div className="flex flex-col m-0">
       <main className="flex flex-col relative z-auto flex-1">
         <div className="loading__screen">
           <div className="absolute left-0 top-0 w-full h-full">
             {/* Video element to display camera feed */}
+
             {cameraAccess && (
               <video
                 ref={videoRef}
@@ -44,45 +53,52 @@ function Aicamera() {
                 className="w-full h-full object-cover"
               />
             )}
-            <span className="dotted-square is-expanded is-animated --size: 19.7vw;">
+
+            <span
+              className="dotted-square is-expanded is-animated"
+              style={{ "--size": "19.7vw" }}
+            >
               {" "}
             </span>
+
             <div className="absolute left-1/2 top-1/2 max-w-[227px] text-center transform -translate-x-1/2 -translate-y-1/2 font-semibold">
-              {cameraAccess === false
-                ? "CAMERA ACCESS IS REQUIRED!"
-                : ""}
+              {cameraAccess === false ? "CAMERA ACCESS IS REQUIRED!" : ""}
             </div>
           </div>
 
           {cameraAccess === true && (
             <>
-             <button className="absolute right-6 top-1/2 flex items-center align-items justify-center w-16 h-16 bg-white text-green-500 rounded-full shadow-lg onClick={startCountdown}">
-                        <svg
-                            className="w-[24px] h-[24px] relative -top-1 left-1 transform"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            role="img"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        >
-                            <path
-                                d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"
-                                fill="#1A1B1C"
-                            ></path>
-                            <circle cx="12" cy="12" r="2"></circle>
-                        </svg>     
-                                   
-                    </button>
-                    <span className="absolute right-6 top-[600px] flex items-center justify-center">TAKE PICTURE</span>      
+              <button
+                className="absolute right-6 top-1/2 flex items-center justify-center w-16 h-16 bg-white text-green-500 rounded-full shadow-lg"
+                onClick={startCountdown}
+              >
+                <svg
+                  className="w-6 h-6"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  role="img"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path
+                    d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"
+                    fill="#1A1B1C"
+                  ></path>
 
-                    
-                             
-            
+                  <circle cx="12" cy="12" r="2"></circle>
+                </svg>
+              </button>
+
+              <span
+                className="absolute right-6 flex items-center justify-center text-center w-16"
+                style={{ top: "calc(50% + 80px)" }}
+              >
+                TAKE PICTURE
+              </span>
             </>
-          )}        
-
+          )}
         </div>
       </main>
     </div>
