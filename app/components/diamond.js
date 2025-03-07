4fee6a8f25b44899f57c77863dc0344d33768371
Kat <kat.m.deans@gmail.com>
2025-03-06 22:15:31 -0500

    Diamond component

---
+++ app/components/diamond.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Diamond = ({
+  size = 'w-[350px] h-[350px]', 
+  dotted = true,                
+  borderColorClass = 'border-black',
+  className = '',
+  ...rest
+}) => {
+  return (
+    <div
+      className={classNames(
+        size,
+        'border',
+        dotted ? 'border-dotted' : 'border-solid',
+        borderColorClass,
+        'rotate-45', 
+        className
+      )}
+      {...rest}
+    />
+  );
+};
